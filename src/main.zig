a32c4e20f1238b05adf81749cb658680050bbffb
Ritik Raj <ritikraj2699@gmail.com>
2026-04-07 00:42:14 +0530

    spawn threads for concurrent connection

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,17 @@ const std = @import("std");
 const stdout = std.fs.File.stdout();
 const net = std.net;
 
+fn handleConnection(connection: net.Server.Connection) void {
+    defer connection.stream.close();
+
+    var buf: [1024]u8 = undefined;
+    while (true) {
+        const n = connection.stream.read(&buf) catch break;
+        if (n == 0) break;
+        connection.stream.writeAll("+PONG\r\n") catch break;
+    }
+}
+
 pub fn main() !void {
     const address = try net.Address.resolveIp("127.0.0.1", 6379);
 
@@ -15,12 +26,7 @@ pub fn main() !void {
 
         try stdout.writeAll("accepted new connection");
 
-        var buf: [1024]u8 = undefined;
-        while (true) {
-            const n = connection.stream.read(&buf) catch break;
-            if (n == 0) break;
-            try connection.stream.writeAll("+PONG\r\n");
-        }
-        connection.stream.close();
+        const thread = try std.Thread.spawn(.{}, handleConnection, .{connection});
+        thread.detach();
     }
 }
